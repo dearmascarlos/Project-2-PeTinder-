98a3ca21ab08783916f4ba0eded0db871337899b
carlos <correo.carlosdearmas@gmail.com>
2022-10-20 11:20:16 +0100

    User controllers getAllUsers finished

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,15 +1,18 @@
 const router = require('express').Router()  // Lo dejo listo para luego pasar las rutas
 
-const usersRouter = require('./user')
+
 const authRouter = require('./auth')
+const userRouter = require('./user')
+const petRouter = require('./pet')
+const breedRouter = require('./breed')
+const dateRouter = require('./date')
 
-router.use('/user', usersRouter)
+
+router.use('/user', userRouter)
 router.use('/auth', authRouter)
-
-
-
-
-
+router.use('/pet', petRouter)
+router.use('/breed', breedRouter)
+router.use('/date', dateRouter)
 
 
 module.exports = router
