9316bb2b0008dc53021c5da7d0c6baa928d2ffcf
carlos <correo.carlosdearmas@gmail.com>
2022-10-20 20:56:22 +0100

    addFriend function finished

--- api/controllers/pet.js
+++ api/controllers/pet.js
@@ -13,8 +13,27 @@ async function createPet(req, res) {
 
 async function seeAllPets(req, res) {
     try {
-        const pets = await Pet.findAll()
+        const pets = await Pet.findAll({
+            include: [{model: Pet, as: 'Friend'}], 
+            attributes: {
+                exclude: ['updatedAt', 'createdAt']
+            },
+            
+        })
         return res.status(200).json(pets)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+async function addFriend(req, res) {
+    try {
+        const pet = await Pet.findByPk(req.params.petId)
+        const friend = await Pet.findByPk(req.params.friendId)
+
+        await pet.addFriend(friend)
+        await friend.addFriend(pet)
+        return res.status(200).send('Friend Added')
     } catch (error) {
         return res.status(500).send(error.message)
     }
@@ -23,6 +42,4 @@ async function seeAllPets(req, res) {
 
 
 
-
-
-module.exports = { createPet, seeAllPets }
+module.exports = { createPet, seeAllPets, addFriend }
--- api/routes/pet.js
+++ api/routes/pet.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 
-const { createPet, seeAllPets } = require('../controllers/pet')
+const { createPet, seeAllPets, addFriend } = require('../controllers/pet')
 const { checkAdmin, checkAuth } = require('../utils')
 
 router.post('/', checkAuth, checkAdmin, createPet)
 router.get('/', checkAuth, seeAllPets)
+router.put('/:petId/friend/:friendId', checkAuth, addFriend)
 
 module.exports = router
