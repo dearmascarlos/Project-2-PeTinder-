332602dac9bb38895c94e3ecf5c6c10acbf95c57
carlos <correo.carlosdearmas@gmail.com>
2022-10-26 19:46:01 +0100

    connect to elephant

--- database/index.js
+++ database/index.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    port: process.env.DB_PORT,
+const sequelize = new Sequelize(`${process.env.DIALECT}://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}`, {
     logging: false
 } )
 
